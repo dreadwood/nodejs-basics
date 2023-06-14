a18cbee13449fc058dea6a15a2243ef10e31dab1
srg <skabassjob@gmail.com>
2023-06-15 02:23:36 +0400

    feat: implement streams tasks

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,17 @@
-const read = async () => {
-    // Write your code here 
-};
+import { createReadStream } from 'node:fs'
+import { stdout } from 'node:process'
+import { fileURLToPath } from 'node:url'
+import path from 'node:path'
 
-await read();
+const FILE_NAME = 'fileToRead.txt'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const resolvedPath = path.join(__dirname, 'files', FILE_NAME)
+
+const read = async () => {
+  const stream = await createReadStream(resolvedPath)
+  await stream.pipe(stdout)
+}
+
+await read()
--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,19 @@
-const transform = async () => {
-    // Write your code here 
-};
+import { stdout, stdin } from 'node:process'
+import { Transform } from 'node:stream'
 
-await transform();
+const reverseTransform = new Transform({
+  transform(chunk, _, callback) {
+    const chunkStr = chunk.toString().trim()
+    const reversedChunk = chunkStr.split('').reverse().join('')
+
+    callback(null, reversedChunk + '\n')
+  },
+})
+
+const transform = async () => {
+  stdin.pipe(reverseTransform).pipe(stdout)
+
+  console.log('Press command-D, control-D, ^-D, Ctrl-D to exit\n')
+}
+
+await transform()
