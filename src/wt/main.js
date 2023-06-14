59e4a32cdee63b92884c164a4b4221587a5d7352
srg <skabassjob@gmail.com>
2023-06-15 02:44:14 +0400

    feat: implement worker threads task

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,47 @@
-const performCalculations = async () => {
-    // Write your code here
-};
+import { Worker } from 'node:worker_threads'
+import os from 'node:os'
+import { fileURLToPath } from 'node:url'
+import path from 'node:path'
 
-await performCalculations();
+const FILE_NAME = 'worker.js'
+const INITIAL_WORKER_DATA = 10
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const resolvedPath = path.join(__dirname, FILE_NAME)
+
+const createWorker = workerData => {
+  return new Promise((resolve, reject) => {
+    const worker = new Worker(resolvedPath, { workerData })
+
+    worker.on('message', data =>
+      resolve({
+        status: 'resolved',
+        data,
+      })
+    )
+
+    worker.on('error', () =>
+      reject({
+        status: 'error',
+        data: null,
+      })
+    )
+  })
+}
+const performCalculations = async () => {
+  const cpus = os.cpus()
+
+  const workers = cpus.map((_, i) => {
+    return createWorker(INITIAL_WORKER_DATA + i)
+  })
+
+  const resultPromise = await Promise.allSettled(workers)
+  const result = resultPromise.map(it => {
+    return it.value || it.reason
+  })
+
+  console.log(result)
+}
+
+await performCalculations()
